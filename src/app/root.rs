c15743e083e36d861371905c43e2ea2928c51a86
Edinson Jim <4842086+edinsonjim@users.noreply.github.com>
2026-04-05 01:33:43 +0200

    feat: add Tailwind CSS integration

--- src/app/root.rs
+++ src/app/root.rs
@@ -3,6 +3,6 @@ use leptos::prelude::*;
 #[component]
 pub fn App() -> impl IntoView {
     view! {
-        <h1>F5A Leptos</h1>
+        <h1 class="text-2xl font-extrabold text-blue-950">F5A Leptos</h1>
     }
 }
